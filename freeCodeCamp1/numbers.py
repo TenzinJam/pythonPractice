bb5f42dae2a53ca12acbbfd7edf631f14459272c
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-28 23:32:38 -0400

    number methods

--- freeCodeCamp1/numbers.py
+++ freeCodeCamp1/numbers.py
@@ -1,1 +1,17 @@
+#importing math library from python to use some of the built-in number methods
+from math import *
+
+
 print(2)
+my_num = -5
+print(my_num)
+print(str(my_num))
+print(str(my_num) == "str")
+# In python, string and number concatination is not allowed. So you would have to stringify first or numberfy first before concatinating them together.
+print(abs(my_num))
+print(pow(3,2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.7))
+print(floor(3.7))
+print(sqrt(36))

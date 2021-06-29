430de03f78cb0288f64d4eb10d9daadebd7ae927
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-29 12:58:14 -0400

    funcions

---
+++ freeCodeCamp1/functions.py
@@ -0,0 +1,6 @@
+#def is the key word for function
+
+def say_hi(name):
+    print("Hello "+name)
+
+say_hi("Mike")
---
+++ freeCodeCamp1/tuples.py
@@ -0,0 +1,7 @@
+#tuple is similar to a list with a few key differences
+#tuples are immutable
+# used in cases where you dont need to change the data
+
+coordinates = (4, 5)
+coordinates1 = [(4, 5),(10, 20),(15, 30)]
+print(coordinates[1])

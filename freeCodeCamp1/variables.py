656ae56c784b7bdc1ee3897b8504b9209c14d9ab
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-28 23:00:13 -0400

    variables in python

---
+++ freeCodeCamp1/variables.py
@@ -0,0 +1,7 @@
+character_name = "John"
+character_age = "35"
+print("There once was a man named "+ character_name + ", ")
+print("he was " + character_age + " years old.")
+print("he really liked the name "+ character_name + ", ")
+print("but didnt like being "+ character_name + ".")
+

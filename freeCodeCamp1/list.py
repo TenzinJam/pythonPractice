ba09c4602285405f5dcfddeac8928339f1a588c3
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-29 12:39:22 -0400

    list methods are here. Had some issue trying to use the latest python version. When you start a command by python, it takes the default version of 2.something. So if you wanna use the most recent, type python3, or whatever the latest big update number is, to run it on that particular environment. As of now, reverse and sort do not seems to work the same way as the one on tutorial. It requires a couple of more steps to print the sorted and reversed list.

--- freeCodeCamp1/list.py
+++ freeCodeCamp1/list.py
@@ -8,4 +8,23 @@ print(friends[1:3])
 friends[1] = "Daniel"
 print(friends[1])
 
+
+# append, extend, insert, remove, clear, pop, count, index, sort
 lucky_number = [4, 8, 15, 16, 23, 42]
+friends.append("Tashi")
+friends.insert(1, "Lhamo")
+friends2 = friends.copy()
+print("new list", friends2)
+name_sorted = friends.sort()
+print("sorted", name_sorted)
+print(friends.index("Tashi"))
+print(friends.count("Lhamo"))
+friends.remove("Jim")
+friends.clear()
+friends.extend(lucky_number)
+friends.pop()
+number_sorted = lucky_number.sort()
+print(lucky_number.reverse())
+print(number_sorted)
+print(friends)
+

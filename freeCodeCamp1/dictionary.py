4b216a8ebdc98e0141a4786aa2243db053a752d6
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-29 17:08:50 -0400

    python dictionary

---
+++ freeCodeCamp1/dictionary.py
@@ -0,0 +1,15 @@
+#pretty similar to js objects. On top, you can get the value by dictionary.get("key")
+
+monthConversions = {
+  "Jan": "January",
+  "Feb": "February",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July",
+
+}
+
+print(monthConversions["Mar"])
+print(monthConversions.get("Apr"))

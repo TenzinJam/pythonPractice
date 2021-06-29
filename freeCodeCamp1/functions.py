abad730af35c385c1717a273cd2ccdce278e0856
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-29 17:02:17 -0400

    more if statements

--- freeCodeCamp1/functions.py
+++ freeCodeCamp1/functions.py
@@ -1,16 +1,16 @@
 #def is the key word for function
-# if statements are constructed using literal english words such as "or" "and" "not"
+
 def say_hi(name):
     print("Hello "+name)
 
 say_hi("Mike")
-
 
 def cube(num):
     return num*num*num
 
 print(cube(3))
 
+# if statements are constructed using literal english words such as "or" "and" "not"
 is_male = True
 is_tall = True
 
@@ -22,3 +22,29 @@ elif not(is_male) and is_tall:
   print("You are just tall")
 else:
   print("You are either not male or not tall or both")
+
+#comparison in python. Same as js, but in python we use double equals.
+
+def max_num(num1, num2, num3):
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  return num3
+
+print(max_num(2,4,5))
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num3 = float(input("Enter second number: "))
+
+if op == "+":
+  print(num1+num3)
+elif op == "-":
+  print(num1-num3)
+elif op == "/":
+  print(num1/num3)
+elif op == "*":
+  print(num1*num3)
+else:
+  print("Invalid Operator")

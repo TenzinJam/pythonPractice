d41dfe727937b3d7d4399c969d31bec1e09e3453
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-29 12:00:17 -0400

    working with lists

---
+++ freeCodeCamp1/list.py
@@ -0,0 +1,11 @@
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Tenzin"]
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:3])
+friends[1] = "Daniel"
+print(friends[1])
+
+lucky_number = [4, 8, 15, 16, 23, 42]

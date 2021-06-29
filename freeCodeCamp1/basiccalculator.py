7cb1a8181a62d857e84563745cb766d3500129bf
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-28 23:44:53 -0400

    basic calculator. The tutorial was made in 2018 and we are asked to specify the type of the number data. However, with the version that my program is runnig on, these specification do not seem necessary

---
+++ freeCodeCamp1/basiccalculator.py
@@ -0,0 +1,8 @@
+
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = num1 + num2
+# result = int(num1) + int(num2)  Looks like with the new python version, you don't need to do this anymore
+#or this: result = float(num1) + float(num2)
+print(result)

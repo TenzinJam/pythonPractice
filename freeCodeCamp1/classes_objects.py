22f680c1b2863a12f413603f4d1f9f8555594747
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-30 13:12:02 -0400

    inheritance is very similar to that in js, but with much simpler syntax

--- freeCodeCamp1/classes_objects.py
+++ freeCodeCamp1/classes_objects.py
@@ -2,7 +2,7 @@
 #self is the js equivalent of "this"
 # comma after each attributes should not be used
 # is_on_probation property was deleted for future exercise
-
+# so the syntax of inheritance is a little different from js, there is no "extends" equivalent keyword and no "super()" function we need to add. All we need to do is input the parent class into the parameters of the child class.
 class Student:
 
   def __init__(self, name, major, gpa):
@@ -16,4 +16,18 @@ class Student:
     else:
       return False
 
+
+class Chef:
+  def make_chicken(self):
+    print("The check makes a chicken")
+  def make_salad(self):
+    print("The check makes a salad")
+  def make_special_dish(self):
+    print("The check makes bbq ribs")
+
+class ChineseChef(Chef):
+  def make_special_dish(self):
+    print("The check makes orange chicken")
+  def make_friend_rice(self):
+    print("The check makes fried rice")
 
---
+++ freeCodeCamp1/inheritance.py
@@ -0,0 +1,12 @@
+from classes_objects import Chef
+from classes_objects import ChineseChef
+
+# we are talking about inheritance, but this is equivalent to extend class in js
+
+
+my_chef = Chef()
+my_chef.make_chicken()
+
+
+my_chinese_chef = ChineseChef()
+my_chinese_chef.make_special_dish()

fba7f626c9707fc5d71fbd2f2c81939ed2af7441
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-30 12:52:26 -0400

    class functions

--- freeCodeCamp1/classes_objects.py
+++ freeCodeCamp1/classes_objects.py
@@ -1,12 +1,19 @@
 # with Class you can define a datatype you want to implement
 #self is the js equivalent of "this"
 # comma after each attributes should not be used
+# is_on_probation property was deleted for future exercise
+
 class Student:
 
-  def __init__(self, name, major, gpa, is_on_probation):
+  def __init__(self, name, major, gpa):
     self.name = name
     self.major = major
     self.gpa = gpa
-    self.is_on_probation = is_on_probation
+
+  def on_honor_roll(self):
+    if self.gpa >= 3.5:
+      return True
+    else:
+      return False
 
 
---
+++ freeCodeCamp1/object_functions.py
@@ -0,0 +1,7 @@
+from classes_objects import Student
+
+
+student1 = Student("Oscar", "Accounting", 3.5)
+student2 = Student("Megan", "Business", 3.8)
+
+print(student1.on_honor_roll())
